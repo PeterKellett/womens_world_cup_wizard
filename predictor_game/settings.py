a33af44556e132a0955131eb8260a78d572739ee
PeterKellett <peterwkellett@gmail.com>
2022-11-10 22:33:34 +0000

    Deploy to Heroku

--- predictor_game/settings.py
+++ predictor_game/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = os.environ.get('PREDICTOR_GAME_SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gitpod.io',
-                 'localhost']
+                 'localhost',
+                 'world-cup-wizard.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io']
 
@@ -124,14 +126,17 @@ WSGI_APPLICATION = 'predictor_game.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_datebase_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }  
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
