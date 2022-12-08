12f390402ca7fc717d58618a7bbf9ae12d778403
PeterKellett <peterwkellett@gmail.com>
2022-12-08 12:19:02 +0000

    Adding email service to app

--- predictor_game/settings.py
+++ predictor_game/settings.py
@@ -114,7 +114,8 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'worldcupwizards@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = TrueEMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
