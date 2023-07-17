44b3f156d0245c809b4b245f2dea8e2505b1439c
PeterKellett <peterwkellett@gmail.com>
2023-07-17 22:11:34 +0000

    Set email vars

--- predictor_game/settings.py
+++ predictor_game/settings.py
@@ -118,7 +118,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'gsmtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('WCW_EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('WCW_EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('WCW_EMAIL_HOST_USER')
